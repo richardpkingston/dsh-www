dabada9ac9c9150824e9a4425ac0aed660c686b1
Richard Kingston <richard.kingston@manchester.ac.uk>
2022-03-11 17:44:35 +0000

    Add files via upload

---
+++ team.php
@@ -0,0 +1,277 @@
+<?php include('header.php'); ?>
+<!-- Begin page content -->
+<main role="main" class="container">
+    <h2 class="card-header mt-2">Meet the team</h2>
+    <div class="container mt-3">
+        <div class="row">
+            <div class="card-deck">
+                <div class="card mb-4" style="min-width: 18rem; max-width: 18rem;">
+                    <!--img class="card-img-top" src="img/" alt="Card image cap"-->
+                    <div class="card-body">
+                        <h5 class="card-title">Prof. Richard Kingston</h5>
+                        <p class="card-text">Planning &amp; Environmental Management</p>
+                        <!--p class="card-text">bio</p-->
+                        <p class="card-text "><a href="#" class="text-white">More to follow</a></p>
+                    </div>
+                </div>
+                <div class="card mb-4" style="min-width: 18rem; max-width: 18rem;">
+                    <div class="card-body">
+                        <h5 class="card-title">Prof. John Ainsworth</h5>
+                        <p class="card-text">Bioinformatics</p>
+                        <!--p class="card-text">bio</p-->
+                        <p class="card-text"><a href="#" class="text-white">More to follow</a></p>
+                    </div>
+                </div>
+                <div class="card mb-4" style="min-width: 18rem; max-width: 18rem;">
+                    <div class="card-body">
+                        <h5 class="card-title">Prof. Andy Brass</h5>
+                        <p class="card-text">Bioinformatics</p>
+                        <!--p class="card-text">bio</p-->
+                        <p class="card-text"><a href="#" class="text-white">More to follow</a></p>
+                    </div>
+                </div>
+                <div class="card mb-4" style="min-width: 18rem; max-width: 18rem;">
+                    <div class="card-body">
+                        <h5 class="card-title">Prof. Hugh Coe</h5>
+                        <p class="card-text">Earth & Environmental Sciences</p>
+                        <!--p class="card-text">bio</p-->
+                        <p class="card-text"><a href="#" class="text-white">More to follow</a></p>
+                    </div>
+                </div>
+                <div class="card mb-4" style="min-width: 18rem; max-width: 18rem;">
+                    <div class="card-body">
+                        <h5 class="card-title">Prof. James Evans</h5>
+                        <p class="card-text">Geography</p>
+                        <!--p class="card-text">bio</p-->
+                        <p class="card-text"><a href="#" class="text-white">More to follow</a></p>
+                    </div>
+                </div>
+                <div class="card mb-4" style="min-width: 18rem; max-width: 18rem;">
+                    <div class="card-body">
+                        <h5 class="card-title">Prof. Caroline Jay</h5>
+                        <p class="card-text">Computer Science</p>
+                        <!--p class="card-text">bio</p-->
+                        <p class="card-text"><a href="#" class="text-white">More to follow</a></p>
+                    </div>
+                </div>
+                <div class="card mb-4" style="min-width: 18rem; max-width: 18rem;">
+                    <div class="card-body">
+                        <h5 class="card-title">Prof. Sarah Lindley</h5>
+                        <p class="card-text">Geography</p>
+                        <!--p class="card-text">bio</p-->
+                        <p class="card-text"><a href="#" class="text-white">More to follow</a></p>
+                    </div>
+                </div>
+                <div class="card mb-4" style="min-width: 18rem; max-width: 18rem;">
+                    <div class="card-body">
+                        <h5 class="card-title">Dr Maria Sharmina</h5>
+                        <p class="card-text">Tyndall Centre</p>
+                        <!--p class="card-text">bio</p-->
+                        <p class="card-text"><a href="#" class="text-white">More to follow</a></p>
+                    </div>
+                </div>
+                <div class="card mb-4" style="min-width: 18rem; max-width: 18rem;">
+                    <div class="card-body">
+                        <h5 class="card-title">Prof. Dave Topping</h5>
+                        <p class="card-text">Earth & Environmental Sciences</p>
+                        <!--p class="card-text">bio</p-->
+                        <p class="card-text"><a href="#" class="text-white">More to follow</a></p>
+                    </div>
+                </div>
+                <div class="card mb-4" style="min-width: 18rem; max-width: 18rem;">
+                    <!--img class="card-img-top" src="img/" alt="Card image cap"-->
+                    <div class="card-body">
+                        <h5 class="card-title">Project Manager - TBC</h5>
+                        <p class="card-text">Digital Solutions</p>
+                        <!--p class="card-text">bio</p-->
+                        <p class="card-text "><a href="#" class="text-white">More to follow</a></p>
+                    </div>
+                </div>
+                <div class="card mb-4" style="min-width: 18rem; max-width: 18rem;">
+                    <!--img class="card-img-top" src="img/" alt="Card image cap"-->
+                    <div class="card-body">
+                        <h5 class="card-title">Systems Architect - TBC</h5>
+                        <p class="card-text">Research IT</p>
+                        <!--p class="card-text">bio</p-->
+                        <p class="card-text "><a href="#" class="text-white">More to follow</a></p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <h1 class="card-header mt-2">Post-doc Researchers</h1>
+    <div class="container mt-3">
+        <div class="row">
+            <div class="card-deck">
+                <div class="card mb-4" style="min-width: 18rem; max-width: 18rem;">
+                    <!--img class="card-img-top" src="img/" alt="Card image cap"-->
+                    <div class="card-body">
+                        <h5 class="card-title">Dr Nourhan Heysham</h5>
+                        <p class="card-text">Digital Solutions</p>
+                        <!--p class="card-text">bio</p-->
+                        <p class="card-text "><a href="#" class="text-white">More to follow</a></p>
+                    </div>
+                </div>
+                <div class="card mb-4" style="min-width: 18rem; max-width: 18rem;">
+                    <!--img class="card-img-top" src="img/" alt="Card image cap"-->
+                    <div class="card-body">
+                        <h5 class="card-title">Dr Moozhan Shakeri</h5>
+                        <p class="card-text">Digital Solutions</p>
+                        <!--p class="card-text">bio</p-->
+                        <p class="card-text "><a href="#" class="text-white">More to follow</a></p>
+                    </div>
+                </div>
+                <div class="card mb-4" style="min-width: 18rem; max-width: 18rem;">
+                    <!--img class="card-img-top" src="img/" alt="Card image cap"-->
+                    <div class="card-body">
+                        <h5 class="card-title">TBC</h5>
+                        <p class="card-text">Digital Solutions</p>
+                        <!--p class="card-text">bio</p-->
+                        <p class="card-text "><a href="#" class="text-white">More to follow</a></p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <h1 class="card-header mt-2">Research Software Engineers</h1>
+    <div class="container mt-3">
+        <div class="row">
+            <div class="card-deck">
+                <div class="card mb-4" style="min-width: 18rem; max-width: 18rem;">
+                    <!--img class="card-img-top" src="img/" alt="Card image cap"-->
+                    <div class="card-body">
+                        <h5 class="card-title">Dr Ann Gledson</h5>
+                        <p class="card-text">Senior RSE lead, Research IT</p>
+                        <!--p class="card-text">bio</p-->
+                        <p class="card-text "><a href="#" class="text-white">More to follow</a></p>
+                    </div>
+                </div>
+                <div class="card mb-4" style="min-width: 18rem; max-width: 18rem;">
+                    <!--img class="card-img-top" src="img/" alt="Card image cap"-->
+                    <div class="card-body">
+                        <h5 class="card-title">Dr Mike Jones</h5>
+                        <p class="card-text">Research IT</p>
+                        <!--p class="card-text">bio</p-->
+                        <p class="card-text "><a href="#" class="text-white">More to follow</a></p>
+                    </div>
+                </div>
+                <div class="card mb-4" style="min-width: 18rem; max-width: 18rem;">
+                    <!--img class="card-img-top" src="img/" alt="Card image cap"-->
+                    <div class="card-body">
+                        <h5 class="card-title">Vasilis Vlastaras</h5>
+                        <p class="card-text">Research IT</p>
+                        <!--p class="card-text">bio</p-->
+                        <p class="card-text "><a href="#" class="text-white">More to follow</a></p>
+                    </div>
+                </div>
+                <div class="card mb-4" style="min-width: 18rem; max-width: 18rem;">
+                    <!--img class="card-img-top" src="img/" alt="Card image cap"-->
+                    <div class="card-body">
+                        <h5 class="card-title">TBC</h5>
+                        <p class="card-text">Research IT</p>
+                        <!--p class="card-text">bio</p-->
+                        <p class="card-text "><a href="#" class="text-white">More to follow</a></p>
+                    </div>
+                </div>
+                <div class="card mb-4" style="min-width: 18rem; max-width: 18rem;">
+                    <!--img class="card-img-top" src="img/" alt="Card image cap"-->
+                    <div class="card-body">
+                        <h5 class="card-title">TBC</h5>
+                        <p class="card-text">Research IT</p>
+                        <!--p class="card-text">bio</p-->
+                        <p class="card-text "><a href="#" class="text-white">More to follow</a></p>
+                    </div>
+                </div>
+                <div class="card mb-4" style="min-width: 18rem; max-width: 18rem;">
+                    <!--img class="card-img-top" src="img/" alt="Card image cap"-->
+                    <div class="card-body">
+                        <h5 class="card-title">TBC</h5>
+                        <p class="card-text">Research IT</p>
+                        <!--p class="card-text">bio</p-->
+                        <p class="card-text "><a href="#" class="text-white">More to follow</a></p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <h1 class="card-header mt-2">Funded partners</h1>
+    <div class="container mt-3">
+        <div class="row">
+            <div class="card-deck">
+                <div class="card mb-4" style="min-width: 18rem; max-width: 18rem;">
+                    <!--img class="card-img-top" src="img/" alt="Card image cap"-->
+                    <div class="card-body">
+                        <h5 class="card-title">NERC's Environmental Data Service</h5>
+                        <p class="card-text"></p>
+                        <!--p class="card-text">bio</p-->
+                        <p class="card-text "><a href="#" class="text-white">More to follow</a></p>
+                    </div>
+                </div>
+                <div class="card mb-4" style="min-width: 18rem; max-width: 18rem;">
+                    <!--img class="card-img-top" src="img/" alt="Card image cap"-->
+                    <div class="card-body">
+                        <h5 class="card-title">Urban Observatory</h5>
+                        <p class="card-text">Newcastle University</p>
+                        <!--p class="card-text">bio</p-->
+                        <p class="card-text "><a href="#" class="text-white">More to follow</a></p>
+                    </div>
+                </div>
+                <div class="card mb-4" style="min-width: 18rem; max-width: 18rem;">
+                    <!--img class="card-img-top" src="img/" alt="Card image cap"-->
+                    <div class="card-body">
+                        <h5 class="card-title">Software Sustainability Institute</h5>
+                        <p class="card-text">University of Southampton</p>
+                        <!--p class="card-text">bio</p-->
+                        <p class="card-text "><a href="#" class="text-white">More to follow</a></p>
+                    </div>
+                </div>
+                <div class="card mb-4" style="min-width: 18rem; max-width: 18rem;">
+                    <!--img class="card-img-top" src="img/" alt="Card image cap"-->
+                    <div class="card-body">
+                        <h5 class="card-title">UKCEH</h5>
+                        <p class="card-text">Data Labs</p>
+                        <!--p class="card-text">bio</p-->
+                        <p class="card-text "><a href="#" class="text-white">More to follow</a></p>
+                    </div>
+                </div>
+                <div class="card mb-4" style="min-width: 18rem; max-width: 18rem;">
+                    <!--img class="card-img-top" src="img/" alt="Card image cap"-->
+                    <div class="card-body">
+                        <h5 class="card-title">WISERD</h5>
+                        <p class="card-text">Cardiff University</p>
+                        <!--p class="card-text">bio</p-->
+                        <p class="card-text "><a href="#" class="text-white">More to follow</a></p>
+                    </div>
+                </div>
+                <div class="card mb-4" style="min-width: 18rem; max-width: 18rem;">
+                    <!--img class="card-img-top" src="img/" alt="Card image cap"-->
+                    <div class="card-body">
+                        <h5 class="card-title">DAFNI</h5>
+                        <p class="card-text">STFC</p>
+                        <!--p class="card-text">bio</p-->
+                        <p class="card-text "><a href="#" class="text-white">More to follow</a></p>
+                    </div>
+                </div>
+                <div class="card mb-4" style="min-width: 18rem; max-width: 18rem;">
+                    <!--img class="card-img-top" src="img/" alt="Card image cap"-->
+                    <div class="card-body">
+                        <h5 class="card-title">Turing Institute</h5>
+                        <p class="card-text">University of Exeter</p>
+                        <!--p class="card-text">bio</p-->
+                        <p class="card-text "><a href="#" class="text-white">More to follow</a></p>
+                    </div>
+                </div>
+                <div class="card mb-4" style="min-width: 18rem; max-width: 18rem;">
+                    <!--img class="card-img-top" src="img/" alt="Card image cap"-->
+                    <div class="card-body">
+                        <h5 class="card-title">Connected Places Catapult</h5>
+                        <p class="card-text"></p>
+                        <!--p class="card-text">bio</p-->
+                        <p class="card-text "><a href="#" class="text-white">More to follow</a></p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</main>
+<?php include('footer.php'); ?>
